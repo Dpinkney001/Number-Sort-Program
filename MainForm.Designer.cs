35c1f768ce4d45316a2072589b2e82ba5fee0dc7
Duvall Pinkney <14938639+Dpinkney001@users.noreply.github.com>
2019-05-15 14:19:22 -0400

    Add files via upload

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Number_Sort_Program_version_2
+﻿namespace Number_Sort_Program_4
 {
     partial class MainForm
     {
@@ -28,67 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
-            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.outputTextBox = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.multiplyArrayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mergeSortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bubbleSortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linearSearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binarySearchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save = new System.Windows.Forms.ToolStripMenuItem();
+            this.writeToFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.Save});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.multiplyArrayToolStripMenuItem,
+            this.mergeSortToolStripMenuItem,
+            this.bubbleSortToolStripMenuItem,
+            this.linearSearchToolStripMenuItem,
+            this.binarySearchToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.toolStripMenuItem1.Text = "Options";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Enter List of numbers";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter list of numbers";
             // 
             // inputTextBox
             // 
-            this.inputTextBox.Location = new System.Drawing.Point(12, 25);
+            this.inputTextBox.Location = new System.Drawing.Point(15, 40);
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(301, 20);
-            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.Size = new System.Drawing.Size(228, 96);
+            this.inputTextBox.TabIndex = 2;
+            this.inputTextBox.Text = "";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Location = new System.Drawing.Point(12, 139);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Output ";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "output";
             // 
             // outputTextBox
             // 
-            this.outputTextBox.Location = new System.Drawing.Point(12, 92);
+            this.outputTextBox.Location = new System.Drawing.Point(15, 155);
             this.outputTextBox.Name = "outputTextBox";
-            this.outputTextBox.Size = new System.Drawing.Size(301, 20);
-            this.outputTextBox.TabIndex = 3;
+            this.outputTextBox.Size = new System.Drawing.Size(228, 96);
+            this.outputTextBox.TabIndex = 4;
+            this.outputTextBox.Text = "";
             // 
-            // label3
+            // multiplyArrayToolStripMenuItem
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(374, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.multiplyArrayToolStripMenuItem.Name = "multiplyArrayToolStripMenuItem";
+            this.multiplyArrayToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.multiplyArrayToolStripMenuItem.Text = "Multiply Array";
+            // 
+            // mergeSortToolStripMenuItem
+            // 
+            this.mergeSortToolStripMenuItem.Name = "mergeSortToolStripMenuItem";
+            this.mergeSortToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mergeSortToolStripMenuItem.Text = "Merge Sort";
+            // 
+            // bubbleSortToolStripMenuItem
+            // 
+            this.bubbleSortToolStripMenuItem.Name = "bubbleSortToolStripMenuItem";
+            this.bubbleSortToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bubbleSortToolStripMenuItem.Text = "Bubble Sort";
+            // 
+            // linearSearchToolStripMenuItem
+            // 
+            this.linearSearchToolStripMenuItem.Name = "linearSearchToolStripMenuItem";
+            this.linearSearchToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.linearSearchToolStripMenuItem.Text = "Linear Search";
+            // 
+            // binarySearchToolStripMenuItem
+            // 
+            this.binarySearchToolStripMenuItem.Name = "binarySearchToolStripMenuItem";
+            this.binarySearchToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.binarySearchToolStripMenuItem.Text = "Binary Search";
+            // 
+            // Save
+            // 
+            this.Save.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.writeToFileToolStripMenuItem});
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(37, 20);
+            this.Save.Text = "File";
+            this.Save.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // writeToFileToolStripMenuItem
+            // 
+            this.writeToFileToolStripMenuItem.Name = "writeToFileToolStripMenuItem";
+            this.writeToFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.writeToFileToolStripMenuItem.Text = "Write to File";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.outputTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Number Sort Program";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,11 +169,19 @@
 
         #endregion
 
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox inputTextBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox outputTextBox;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.ToolStripMenuItem multiplyArrayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mergeSortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bubbleSortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem linearSearchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binarySearchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save;
+        private System.Windows.Forms.ToolStripMenuItem writeToFileToolStripMenuItem;
     }
 }
 
